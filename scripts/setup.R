f581071529a83772b0ccec14bc211ec285aa7409
Tim Schatto-Eckrodt <kudusch@posteo.de>
2025-09-16 14:56:08 +0200

    Add info for private osf repos

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,5 +1,6 @@
 # Download all files form the Reproducible Template OSF project
 # Replace the link to the project with your own!
+# If you are working with a private project, you have to authenticate first: osf_auth()
 osfr::osf_retrieve_node("https://osf.io/avh69/") |> 
     osfr::osf_ls_files(
         type = "file", 
